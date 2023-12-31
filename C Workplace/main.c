f6860fc9df5d166fafa57f4d60d003aa49057307
Mohammed Ahmed Majid <mohammedahmedmajid14@gmail.com>
2023-12-31 15:35:00 +0530

    Initial Commit

---
+++ C Workplace/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C Workplace
+//
+//  Created by Mohammed Ahmed Majid on 31/12/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
